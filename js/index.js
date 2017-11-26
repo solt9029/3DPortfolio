ab689055f34be65e0a6d1a8e72127dfdd6cf269c
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-11-26 22:31:32 +0900

    resize

--- js/index.js
+++ js/index.js
@@ -119,7 +119,9 @@ function initEvent() {
     });
 
     $(window).resize(function(event) {
-        // alert('resize');
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
     });
 }
 
